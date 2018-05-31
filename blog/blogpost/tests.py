0ebb2cce97309ad2451a65c18f64bd332f767de8
Ze Wilsen <wilsem_ze@outlook.com>
2018-05-31 14:19:29 +0800

    add chromedrive.exe

--- blog/blogpost/tests.py
+++ blog/blogpost/tests.py
@@ -5,12 +5,15 @@ from datetime import datetime
 from .views import index, view_post
 from .models import Blogpost
 from selenium import webdriver
+import os
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class HomepageTestCase(LiveServerTestCase):
 
     def setUp(self):
-        self.selenium = webdriver.Chrome()
+        self.selenium = webdriver.Chrome(os.path.join(BASE_DIR, 'chromedriver.exe'))
         self.selenium.maximize_window()
         super().setUp()
 
@@ -20,7 +23,7 @@ class HomepageTestCase(LiveServerTestCase):
 
     def test_visit_homepage(self):
         self.selenium.get(
-            '%s%s' % self.live_server_url, "blogpost/"
+            '%s%s' % (self.live_server_url, "/blogpost/")
         )
 
         self.assertIn("Welcome to my blog", self.selenium.title)
